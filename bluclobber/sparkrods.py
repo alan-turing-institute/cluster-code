1ad648a7494854a501aefd422b810d5c798b3936
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-07-09 17:30:03 +0100

    Updated bluclobber/sparkrods.py to handle both local files and URLs in oids.txt. If local files are specified then streams are opened to these using Python built-in function . get_streams behaves conditionally on whether all oids are prefixed with http://|https:// or are local files.

--- bluclobber/sparkrods.py
+++ bluclobber/sparkrods.py
@@ -8,9 +8,17 @@ def get_streams(downsample=1, source="oids.txt", app="Books"):
 
     oids = map(lambda x: x.strip(), list(open('oids.txt')))
 
+    are_urls = len(oids) > 0 and \
+        (oids[0].lower().startswith("http://") or \
+         oids[0].lower().startswith("https://"))
+
     rddoids = sc.parallelize(oids)
 #    down = rddoids.sample(False, 1.0 / downsample )
     down = rddoids
-    streams = down.map(lambda x:
-                       requests.get(x, stream=True).raw)
-    return streams
+    if (are_urls):
+        streams = down.map(lambda x:
+                           requests.get(x, stream=True).raw)
+        return streams
+    else:  # file paths
+        streams = down.map(lambda x: open(x))
+        return streams
