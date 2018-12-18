5b36e1700d5dfa1a5ba32d8450018c9e4cac863b
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-12-18 16:52:11 +0000

    Removed redundant imports and replaced lambda x,y: x+y with operator.add

--- queries/total_books.py
+++ queries/total_books.py
@@ -1,21 +1,12 @@
-'''
-This module counts the total number of books.
-'''
-
-from operator import add
-import re
-
-from bluclobber.archive import Archive
-from bluclobber.sparkrods import get_streams
-
-import yaml
-import sys
+"""
+Counts the total number of books.
+"""
 
 
 def do_query(archives, _, _log):
-    '''
-    Get total number of books.
-    '''
+    """
+    Counts the total number of books.
+    """
     books = archives.flatMap(lambda archive: list(archive))
     result = books.count()
     return {"books": result}
--- queries/total_words.py
+++ queries/total_words.py
@@ -1,22 +1,15 @@
-'''
-This module counts the total number of words across all books.
-'''
+"""
+Counts the total number of words across all books.
+"""
 
 from operator import add
-import re
-
-from bluclobber.archive import Archive
-from bluclobber.sparkrods import get_streams
-
-import yaml
-import sys
 
 
 def do_query(archives, _, _log):
-    '''
-    Get total number of words across all books.
-    '''
+    """
+    Counts the total number of words across all books.
+    """
     books = archives.flatMap(lambda archive: list(archive))
     word_counts = books.map(lambda book: len(list(book.words())))
-    result = [books.count(), word_counts.reduce(lambda x,y: x+y)]
+    result = [books.count(), word_counts.reduce(add)]
     return {"books": result[0], "words": result[1]}
