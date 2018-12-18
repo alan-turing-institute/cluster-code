157bbd06bd53631081942c183c12f75a161c6a08
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-12-18 14:25:18 +0000

    Aligned setup.py with i_newspaper_rods setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
+"""
+This is the project specification for setuptools
+"""
+
 import os
 from setuptools import setup
 
+
 def read(fname):
+    """
+    Read a file relative the current directory and return its contents
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "bluclobber",
-    version = "0.0.1",
-    author = "James Hetherington",
-    author_email = "j.hetherington@ucl.ac.uk",
-    description = ("Harness for cluster map/reduce analysis of ALTO books corpus"),
-    license = "BSD",
-    keywords = "digital humanities research books",
-    url = "http://development.rc.ucl.ac.uk/",
+    name="bluclobber",
+    version="0.0.1",
+    author="James Hetherington",
+    author_email="j.hetherington@ucl.ac.uk",
+    description=(
+        "Harness for Apache Spark analysis of ALTO books corpus"),
+    license="MIT",
+    keywords="digital humanities research books",
+    url="https://github.com/alan-turing-institute/cluster-code",
     packages=['bluclobber'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Research :: Humanities",
-        "License :: OSI Approved :: BSD License",
-    ],
-    entry_points={
-          'console_scripts': [
-              'bluclobber = bluclobber.harness.query:main',
-              'bluclobber_repartition = bluclobber.harness.repartition:main'
-          ]
-      },
+        "License :: OSI Approved :: MIT License",
+    ]
 )
