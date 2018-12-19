eb7905a9f001467392246617cf58598b28aeffeb
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-12-19 17:42:11 +0000

    Updated queries/find_words_group_by_word.py queries/find_words_group_by_year.py to normalize words using utils.normalize(word) when searching for matches. Allows matches for upper/lower case, hyphenated words etc

--- queries/find_words_group_by_word.py
+++ queries/find_words_group_by_word.py
@@ -1,7 +1,12 @@
 """
 Counts the number of occurrences of words per-year and groups by
-word. The query expects a file with a list of the words to search for,
-one per line.
+word.
+
+The query expects a file with a list of the words to search for, one
+per line.
+
+Words are normalized, by removing all 'a-z|A-Z' characters before
+comparing with the list of words to search for.
 
 The result is of form, for example:
 
@@ -16,7 +21,8 @@ Only words that occur one or more times are returned.
 """
 
 from operator import add
-import re
+
+import utils
 
 
 def do_query(archives, words_file, logger=None):
@@ -31,36 +37,28 @@ def do_query(archives, words_file, logger=None):
     @type words_file: str or unicode
     @param logger: Logger
     """
+    search_words = []
     with open(words_file, "r") as f:
-        words = [re.compile(r'\b' + word.strip() + r'\b', re.I | re.U)
-                 for word in list(f)]
+        search_words = [word.strip() for word in list(f)]
 
     books = archives.flatMap(
         lambda archive: [(book.year, book) for book in list(archive)])
     # [(YEAR, BOOK), ...]
 
-    pages = books.flatMap(
-        lambda (year, book): [(year, page) for page in list(book)])
-    # [(YEAR, PAGE), ...]
+    words = books.flatMap(
+        lambda (year, book): [
+            ((year, utils.normalize(word)), 1)
+            for (_, word) in book.scan_words()
+        ])
+    # [((YEAR, WORD), 1), ...]
 
-    matches = pages.flatMap(
-        lambda (year, page):
-        [((year, regex.pattern), regex.findall(page.content))
-         for regex in words])
-    # [((YEAR, WORD), MATCHES), ...]
-
-    num_matches = matches.mapValues(len)
-    # [((YEAR, WORD), NNNN), ...]
-
-    num_matches = num_matches.filter(
-        lambda (year_word, num_matches): num_matches > 0)
-    # [((YEAR, WORD), NNNN), ...]
+    num_matches = words.filter(
+        lambda ((year, word), _): word in search_words)
+    # [((YEAR, WORD), 1), ...]
 
     result = num_matches \
         .reduceByKey(add) \
-        .map(lambda (year_word, count): (year_word[1].replace(r'\b', ''),
-                                         (year_word[0],
-                                          count))) \
+        .map(lambda ((year, word), count): (word, (year, count))) \
         .groupByKey() \
         .map(lambda (year, data): (year, list(data))) \
         .collect()
