b78b8c64c97178cfbd603eea8a5390919c1ef7f2
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-12-20 09:37:57 +0000

    Removed queries/to_port/mean_pages_by_year.py as covered by queries/total_pages.py

--- queries/to_port/mean_pages_by_year.py
+++
@@ -1,9 +0,0 @@
-# An example analysis, for determining average pages per book
-
-def mapper(book):
-    return {book.year: [1, book.pages]}
-
-reducer=merge_under(double_sum)
-
-def shuffler(year, count):
-    return year%count
