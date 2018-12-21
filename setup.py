b2582a5241fa126970fb394c41d785a007fcb8a0
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-12-21 10:11:47 +0000

    Updated setup.py metadata

--- setup.py
+++ setup.py
@@ -13,11 +13,21 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+AUTHORS = ["James Hetherington",
+           "James Baker",
+           "Mike Jackson",
+           "Rosa Filgueira"]
+
+EMAILS = ["j.hetherington@ucl.ac.uk",
+          "james.baker@bl.uk",
+          "michaelj@epcc.ed.ac.uk",
+          "rosa.filgueira@ed.ac.uk"]
+
 setup(
     name="bluclobber",
     version="0.0.1",
-    author="James Hetherington",
-    author_email="j.hetherington@ucl.ac.uk",
+    authors=", ".join(AUTHORS),
+    author_email=", ".join(EMAILS),
     description=(
         "Harness for Apache Spark analysis of ALTO books corpus"),
     license="MIT",
@@ -26,7 +36,7 @@ setup(
     packages=['bluclobber'],
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Topic :: Research :: Humanities",
         "License :: OSI Approved :: MIT License",
     ]
