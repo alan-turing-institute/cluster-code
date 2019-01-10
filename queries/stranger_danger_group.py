78163a9fa4067716df4a2ed7a6fec016305a3eb5
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-10 15:18:36 +0000

    Added Rosa's modified version of stranger_danger_group.py which avoids overlapping text in the query results

--- queries/stranger_danger_group.py
+++ queries/stranger_danger_group.py
@@ -1,17 +1,28 @@
+from collections import defaultdict
+from collections import deque
+from itertools import islice
 groups=[('stranger','danger')]
 chunk_length=12
 
 def mapper(book):
     finds=defaultdict(list)
-    for finding in groups_of(chunk_length,book.scan_words()):
+    iterable=iter(book.scan_words())
+    finding=deque(islice(iterable,0,chunk_length))
+    for element in iterable:
         for group in groups:
             words=[normalize(word) for page, word in finding]
             contained=[target in words for target in group]
         if all(contained):
             finds[group].append([words, finding[0][0].code])
-    result = {group: [[book.title, book.publisher, book.year, book.code, content]] 
+            for i in range(chunk_length):
+                finding.popleft()
+                element=iterable.next()
+                finding.append(element)
+        else:
+            finding.popleft()
+            finding.append(element)
+    result = {group: [[book.title, book.publisher, book.year, book.code, content]]
             for group, content in finds.iteritems()}
     return result
 
 reducer=merge_under(add)
-
